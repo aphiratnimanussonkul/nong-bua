616a5766ebe19e9b07a0ebd9f5ea0c1cd4f6c1bd
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-01 23:26:08 +0700

    added action handle when click contact me

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,7 +7,29 @@ import PhoneIcon from "@material-ui/icons/Phone";
 import RoomIcon from "@material-ui/icons/Room";
 import MapIcon from "@material-ui/icons/Map";
 import "./footer.scss";
+import { Tooltip } from "@material-ui/core";
 export default function Footer() {
+  const handleActionClick = (menu) => {
+    switch (menu) {
+      case "GOOGLE_MAP":
+        window.open(
+          "https://www.google.com/maps/place/15%C2%B015'29.3%22N+102%C2%B037'04.2%22E/@15.2581442,102.6169093,581m/data=!3m2!1e3!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d15.2581424!4d102.6178196",
+          "_blank"
+        );
+        break;
+      case "GITHUB":
+        window.open(
+          "https://github.com/aphiratnimanussonkul?tab=repositories",
+          "_blank"
+        );
+        break;
+      case "MAIL":
+        navigator.clipboard.writeText("B5923151@gmail.com");
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <>
       <div className="footer">
@@ -25,21 +47,27 @@ export default function Footer() {
               <span>30110</span>
             </p>
           </div>
-          <div className="contact-detail pointer">
+          <div
+            className="contact-detail pointer"
+            onClick={() => handleActionClick("GOOGLE_MAP")}
+          >
             <MapIcon></MapIcon>
             <p>Google Map</p>
           </div>
         </div>
         <div className="line"></div>
         <div className="copyright">
-          <IconButton>
-            <CopyrightIcon />
-          </IconButton>
+          <CopyrightIcon />
           <h2>Aphirat Nimanussonkul</h2>
-          <IconButton>
-            <MailIcon />
-          </IconButton>
-          <IconButton>
+          <Tooltip
+            title="Click to copy B5923151@gmail.com"
+            onClick={() => handleActionClick("MAIL")}
+          >
+            <IconButton id="mail">
+              <MailIcon />
+            </IconButton>
+          </Tooltip>
+          <IconButton onClick={() => handleActionClick("GITHUB")}>
             <GitHubIcon />
           </IconButton>
         </div>
