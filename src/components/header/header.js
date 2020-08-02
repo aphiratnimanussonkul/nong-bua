373475e863c749473d8af8f56a8ef19e60edae2c
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-02 11:22:16 +0700

    change header component to function component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,60 +6,53 @@ import Tab from "@material-ui/core/Tab";
 import { withRouter, useHistory } from "react-router-dom";
 import "./header.scss";
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 0,
-      menus: [
-        {
-          label: "หน้าแรก",
-          link: "/",
-        },
-        {
-          label: "ข่าวและกิจกรรม",
-          link: "/news-information",
-        },
-        {
-          label: "กองทุนหมู่บ้าน",
-          link: "/village-fund",
-        },
-        {
-          label: "อสม",
-          link: "/village-health-volunteer",
-        },
-        {
-          label: "เกี่ยวกับหมู่บ้าน",
-          link: "/about-village",
-        },
-      ],
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = (event, newValue) => {
-    this.state.value = newValue;
-    this.props.history.push(this.state.menus[newValue].link);
+const Header = () => {
+  const history = useHistory();
+  const [value, setValue] = useState(0);
+  const menus = [
+    {
+      label: "หน้าแรก",
+      link: "/",
+    },
+    {
+      label: "ข่าวและกิจกรรม",
+      link: "/news-information",
+    },
+    {
+      label: "กองทุนหมู่บ้าน",
+      link: "/village-fund",
+    },
+    {
+      label: "อสม",
+      link: "/village-health-volunteer",
+    },
+    {
+      label: "เกี่ยวกับหมู่บ้าน",
+      link: "/about-village",
+    },
+  ];
+  
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    history.push(menus[newValue].link);
   };
-  render() {
-    return (
-      <>
-        <AppBar position="static">
-          <Toolbar className="navbar">
-            <h1>หมู่บ้านหนองบัว</h1>
-            <p>หมู่ที่ 25 ตำบลโบสถ์ อำเภอพิมาย จังหวัดนครราชสีมา</p>
-          </Toolbar>
-          <Toolbar className="nav-menu-bar">
-            <Tabs value={this.state.value} onChange={this.handleChange}>
-              {this.state.menus.map((menu) => {
-                return <Tab label={menu.label} />;
-              })}
-            </Tabs>
-          </Toolbar>
-        </AppBar>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar className="navbar">
+          <h1>หมู่บ้านหนองบัว</h1>
+          <p>หมู่ที่ 25 ตำบลโบสถ์ อำเภอพิมาย จังหวัดนครราชสีมา</p>
+        </Toolbar>
+        <Toolbar className="nav-menu-bar">
+          <Tabs value={value} onChange={handleChange}>
+            {menus.map((menu) => {
+              return <Tab label={menu.label} />;
+            })}
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default withRouter(Header);
