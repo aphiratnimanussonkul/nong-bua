a6fdb713f6bc08261f66817484f31d29735f1274
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-02 18:15:07 +0700

    use global css

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import NewsCarousal from "./components/news-carousal/news-carousal.js";
 import AboutVillage from "./components/about-village/about-village.js";
 import "./home.scss";
+import News from "../../components/news/news.js";
 
 const Home = ({}) => {
   return (
@@ -10,6 +11,7 @@ const Home = ({}) => {
       <div className="content">
         <AboutVillage></AboutVillage>
       </div>
+      <News></News>
     </>
   );
 };
