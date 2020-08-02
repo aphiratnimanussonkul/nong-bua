01926c66b2ec3b5cbfe15838dc8854db4f31953b
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-02 18:13:34 +0700

    created and implement global css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Routes from "./routes/index";
 import { withRouter } from "react-router-dom";
+
+import "./theme/global.scss";
+import "./App.css";
 
 const App = () => {
   return (
