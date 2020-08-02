a217b2a0097614a79af4cce09c2bfa1b9230d4aa
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-02 11:08:26 +0700

    create empty home and news-information page

---
+++ src/components/news-information/news-information.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewsInformation = ({}) => {
+  return (
+    <>
+      <p>This is News-Information Page</p>
+    </>
+  );
+};
+
+export default NewsInformation;
