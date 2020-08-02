ecb779a7cffd3ee5cc52ad7e4bc19a7998d39d03
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-02 23:37:58 +0700

    added village-fund in route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../views/home/home.js";
 import NewsInformation from "../views/news-information/news-information.js";
+import VillageFund from "../views/village-fund/village-fund.js";
 
 const Routing = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/news-information" component={NewsInformation} />
+      <Route exact path="/village-fund" component={VillageFund} />
     </Switch>
   );
 };
