a98cac8e04068855b0d704430a6614bba131c600
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-02 11:09:53 +0700

    implement route on header and route when click menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,52 +1,65 @@
-import React from "react";
+import React, { useState, Component } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import { withRouter, useHistory } from "react-router-dom";
 import "./header.scss";
-export default function Header() {
-  const [value, setValue] = React.useState(0);
-  const menus = [
-    {
-      label: "หน้าแรก",
-      link: "/home",
-    },
-    {
-      label: "ข่าวและกิจกรรม",
-      link: "/information",
-    },
-    {
-      label: "กองทุนหมู่บ้าน",
-      link: "/village-fund",
-    },
-    {
-      label: "อสม",
-      link: "/village-health-volunteer",
-    },
-    {
-      label: "เกี่ยวกับหมู่บ้าน",
-      link: "/about-village",
-    },
-  ];
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
-  return (
-    <>
-      <AppBar position="static">
-        <Toolbar className="navbar">
-          <h1>หมู่บ้านหนองบัว</h1>
-          <p>หมู่ที่ 25 ตำบลโบสถ์ อำเภอพิมาย จังหวัดนครราชสีมา</p>
-        </Toolbar>
-        <Toolbar className="nav-menu-bar">
-          <Tabs value={value} onChange={handleChange}>
-            {menus.map((menu) => {
-              return <Tab label={menu.label} />;
-            })}
-          </Tabs>
-        </Toolbar>
-      </AppBar>
-    </>
-  );
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      menus: [
+        {
+          label: "หน้าแรก",
+          link: "/",
+        },
+        {
+          label: "ข่าวและกิจกรรม",
+          link: "/news-information",
+        },
+        {
+          label: "กองทุนหมู่บ้าน",
+          link: "/village-fund",
+        },
+        {
+          label: "อสม",
+          link: "/village-health-volunteer",
+        },
+        {
+          label: "เกี่ยวกับหมู่บ้าน",
+          link: "/about-village",
+        },
+      ],
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event, newValue) => {
+    this.state.value = newValue;
+    this.props.history.push(this.state.menus[newValue].link);
+  };
+  render() {
+    return (
+      <>
+        <AppBar position="static">
+          <Toolbar className="navbar">
+            <h1>หมู่บ้านหนองบัว</h1>
+            <p>หมู่ที่ 25 ตำบลโบสถ์ อำเภอพิมาย จังหวัดนครราชสีมา</p>
+          </Toolbar>
+          <Toolbar className="nav-menu-bar">
+            <Tabs value={this.state.value} onChange={this.handleChange}>
+              {this.state.menus.map((menu) => {
+                return <Tab label={menu.label} />;
+              })}
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+      </>
+    );
+  }
 }
+
+export default withRouter(Header);
