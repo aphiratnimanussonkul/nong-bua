deae7aee7db7140944683bd5dbf80a2de13d37ec
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-02 18:26:35 +0700

    remove hard code data

--- src/components/news/components/news-card/news-card.js
+++ src/components/news/components/news-card/news-card.js
@@ -12,29 +12,27 @@ import {
 } from "@material-ui/core";
 import "./news-card.scss";
 
-const NewsCard = () => {
+const NewsCard = ({ detail }) => {
   return (
     <Card>
       <CardActionArea>
-        <CardMedia
-          image="https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.0-9/52629688_1295604687263387_4952472015299674112_n.jpg?_nc_cat=103&_nc_sid=8bfeb9&_nc_eui2=AeH7VOKMBAH-JFe0rwb7rYn-u04OAlfpv1O7Tg4CV-m_U0BAgpRzrwQSdU-i8QwvmxpRrqk6B69xGKtzH19aaAj-&_nc_ohc=8WSiVI6PvPMAX945_oS&_nc_ht=scontent.fbkk20-1.fna&oh=6356b09bb68e3fd32aa05af3c0e14ab4&oe=5F4AD06D"
-          title="Contemplative Reptile"
-        />
+        <CardMedia image={detail.image} />
         <CardContent>
           <div className="top-card-news">
             <div className="tags">
-              <Chip label="กองทุนหมู่บ้าน" size="small" className="chip" />
+              {detail.tags.map((tag) => {
+                return <Chip label={tag} size="small" className="chip" />;
+              })}
             </div>
             <Typography variant="subtitile2" className="how-long">
-              1 วันที่แล้ว
+              {detail.created_at}
             </Typography>
           </div>
           <Typography gutterBottom variant="h5" className="toppick">
-            กิจกรรม
+            {detail.title}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {detail.description}
           </Typography>
         </CardContent>
       </CardActionArea>
--- src/components/news/news.js
+++ src/components/news/news.js
@@ -1,27 +1,17 @@
 import React from "react";
-import {
-  Typography,
-  Button,
-  CardMedia,
-  CardContent,
-  CardActions,
-  CardActionArea,
-  Card,
-  GridList,
-} from "@material-ui/core";
+import { GridList } from "@material-ui/core";
 import "./news.scss";
 import NewsCard from "./components/news-card/news-card";
 
-const News = () => {
-  const cards = ["assas", "asd", "asdadasd", "asdF"];
-
+const News = ({ news }) => {
+  console.log(news);
   return (
     <div className="news">
       <div className="content">
         <h2 className="toppick">ข่าวและกิจกรรม</h2>
         <GridList>
-          {cards.map((card) => {
-            return <NewsCard></NewsCard>;
+          {news.map((detail) => {
+            return <NewsCard detail={detail}></NewsCard>;
           })}
         </GridList>
       </div>
