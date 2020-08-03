676a1ed6a353524e95bca26c641458b7ab9e08fd
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-03 21:22:39 +0700

    created village-fund component

---
+++ src/views/village-health-volunteer/components/static-card/static-card.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./static-card.scss";
+import { Card, CardContent, Icon } from "@material-ui/core";
+
+const StaticCard = ({ staticData }) => {
+  return (
+    <Card className="static-card">
+      <CardContent>
+        {staticData.iconName ? (
+          <Icon>{staticData.iconName}</Icon>
+        ) : (
+          <img src={staticData.iconUrl} />
+        )}
+        <h1>{staticData.amount}</h1>
+        <h4>{staticData.unit}</h4>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default StaticCard;
