ca1b30a80420bcac13255474e92269e25db50c0b
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-08 16:08:22 +0700

    create function get village introduce

--- src/actions/home.js
+++ src/actions/home.js
@@ -4,6 +4,9 @@ import { createActionSet } from "../helpers/index";
 const { firestore } = firebase;
 
 export const FETCH_NEWS_INFORMATION = createActionSet("FETCH_NEWS_INFORMATION");
+export const FETCH_VILLAGE_INTRODUCE = createActionSet(
+  "FETCH_VILLAGE_INTRODUCE"
+);
 
 export const getNews = () => async (dispatch) => {
   dispatch({ type: FETCH_NEWS_INFORMATION.PENDDING });
@@ -21,5 +24,24 @@ export const getNews = () => async (dispatch) => {
       });
   } catch (error) {
     dispatch({ type: FETCH_NEWS_INFORMATION.FAILED, error });
+  }
+};
+
+export const getVillageIntroduce = () => async (dispatch) => {
+  dispatch({ type: FETCH_VILLAGE_INTRODUCE.PENDDING });
+
+  try {
+    await firestore
+      .collection("about-village")
+      .where("tag", "==", "INTRODUCE")
+      .get()
+      .then((result) => {
+        dispatch({
+          type: FETCH_VILLAGE_INTRODUCE.SUCCESS,
+          payload: result.docs.map((data) => data.data()),
+        });
+      });
+  } catch (error) {
+    dispatch({ type: FETCH_VILLAGE_INTRODUCE.FAILED, error });
   }
 };
