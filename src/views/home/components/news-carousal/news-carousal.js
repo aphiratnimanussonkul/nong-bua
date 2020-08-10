fe289b66733410614532e43a387ea259a6aa4e0f
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-10 16:44:23 +0700

    added action read new on news carousal

--- src/views/home/components/news-carousal/news-carousal.js
+++ src/views/home/components/news-carousal/news-carousal.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 import { CardMedia } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
 import "./news-carousal.scss";
 
 const NewsCarousal = ({ recentlyNews }) => {
+  const history = useHistory();
+  const readNews = (newsId) => {
+    history.push(`/read-news/${newsId}`);
+  };
   return (
     <Carousel>
       {recentlyNews.map((item) => (
-        <CardMedia image={item.images[0]}>
+        <CardMedia image={item.images[0]} onClick={() => readNews(item.id)}>
           <div className="news-title">
             <div className="vertical-divider"></div>
             <h3>{item.title}</h3>
