bd2b696fa94e1098606140349d1f6f2712572294
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-12 10:00:02 +0700

    remove mock data

--- src/views/village-fund/village-fund.js
+++ src/views/village-fund/village-fund.js
@@ -20,36 +20,6 @@ const VillageFund = ({ dispatch, directories, projects, newsInformation }) => {
     dispatch(getVillageNewsInformation());
   }, [dispatch]);
 
-  const news = [
-    {
-      title: "",
-      image:
-        "https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.0-9/52629688_1295604687263387_4952472015299674112_n.jpg?_nc_cat=103&_nc_sid=8bfeb9&_nc_eui2=AeH7VOKMBAH-JFe0rwb7rYn-u04OAlfpv1O7Tg4CV-m_U0BAgpRzrwQSdU-i8QwvmxpRrqk6B69xGKtzH19aaAj-&_nc_ohc=8WSiVI6PvPMAX945_oS&_nc_ht=scontent.fbkk20-1.fna&oh=6356b09bb68e3fd32aa05af3c0e14ab4&oe=5F4AD06D",
-      description:
-        "ส่งงาน รับงาน เรียบร้อยค่ะ จากวันที่รอคอย ได้รับเงินสนับสนุนจากรัฐบาลโครงการประชารัฐ เพื่อเศษฐกิจฐานราก...",
-      tags: ["กองทุนหมู่บ้าน"],
-      created_at: "1 วันที่แล้ว",
-    },
-    {
-      title: "",
-      image:
-        "https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.0-9/52629688_1295604687263387_4952472015299674112_n.jpg?_nc_cat=103&_nc_sid=8bfeb9&_nc_eui2=AeH7VOKMBAH-JFe0rwb7rYn-u04OAlfpv1O7Tg4CV-m_U0BAgpRzrwQSdU-i8QwvmxpRrqk6B69xGKtzH19aaAj-&_nc_ohc=8WSiVI6PvPMAX945_oS&_nc_ht=scontent.fbkk20-1.fna&oh=6356b09bb68e3fd32aa05af3c0e14ab4&oe=5F4AD06D",
-      description:
-        "ส่งงาน รับงาน เรียบร้อยค่ะ จากวันที่รอคอย ได้รับเงินสนับสนุนจากรัฐบาลโครงการประชารัฐ เพื่อเศษฐกิจฐานราก...",
-      tags: ["กองทุนหมู่บ้าน"],
-      created_at: "1 วันที่แล้ว",
-    },
-    {
-      title: "",
-      image:
-        "https://scontent.fbkk20-1.fna.fbcdn.net/v/t1.0-9/52629688_1295604687263387_4952472015299674112_n.jpg?_nc_cat=103&_nc_sid=8bfeb9&_nc_eui2=AeH7VOKMBAH-JFe0rwb7rYn-u04OAlfpv1O7Tg4CV-m_U0BAgpRzrwQSdU-i8QwvmxpRrqk6B69xGKtzH19aaAj-&_nc_ohc=8WSiVI6PvPMAX945_oS&_nc_ht=scontent.fbkk20-1.fna&oh=6356b09bb68e3fd32aa05af3c0e14ab4&oe=5F4AD06D",
-      description:
-        "ส่งงาน รับงาน เรียบร้อยค่ะ จากวันที่รอคอย ได้รับเงินสนับสนุนจากรัฐบาลโครงการประชารัฐ เพื่อเศษฐกิจฐานราก...",
-      tags: ["กองทุนหมู่บ้าน"],
-      created_at: "1 วันที่แล้ว",
-    },
-  ];
-
   return (
     <>
       <div className="directory">
