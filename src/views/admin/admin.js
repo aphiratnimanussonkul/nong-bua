960cb3ade45a05fb09f8e8b005f919ac45bfaa8d
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-12 11:31:27 +0700

    add management routes

--- src/views/admin/admin.js
+++ src/views/admin/admin.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import "./admin.scss";
 import SideNav from "../../components/side-nav/side-nav";
+import ManagementRoutes from "./routes";
+
+import './admin.scss'
 
 const Admin = () => {
   return (
-    <>
+    <div className="management-container">
       <SideNav></SideNav>
-      <div>this is admin</div>
-    </>
+      <ManagementRoutes></ManagementRoutes>
+    </div>
   );
 };
 
---
+++ src/views/admin/routes/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import NewsManage from "../components/news-manage/news-manage";
+
+const ManagementRoutes = () => {
+  return (
+    <Switch>
+      <Route exact path="/management/news-manage" component={NewsManage} />
+    </Switch>
+  );
+};
+
+export default ManagementRoutes;
