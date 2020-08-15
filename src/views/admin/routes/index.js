9df4d997f635e726e1a1fd080ff6d5978eb9181c
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-15 17:17:45 +0700

    created village-health-volunteer-directory and add in route

--- src/views/admin/routes/index.js
+++ src/views/admin/routes/index.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import NewsManage from "../components/news-manage/news-manage";
 import VillageFundDirectory from "../components/village-fund-directory/village-fund-directory";
 import VillageFundPorject from "../components/village-fund-project/village-fund-project";
+import VillageHealthVolunteerDirectory from "../components/village-health-volunteer-directory/village-health-volunteer-directory";
 
 const ManagementRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const ManagementRoutes = () => {
       <Route exact path="/management/news-manage" component={NewsManage} />
       <Route exact path="/management/village-fund-directory" component={VillageFundDirectory} />
       <Route exact path="/management/village-fund-project" component={VillageFundPorject} />
+      <Route exact path="/management/village-health-volunteer" component={VillageHealthVolunteerDirectory} />
     </Switch>
   );
 };
