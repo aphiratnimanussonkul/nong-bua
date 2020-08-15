f2f3b67837339d1bf269f35b06add6c585dbdcc1
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-15 14:38:16 +0700

    create empty village fund directory component and add to manangement route

---
+++ src/views/admin/components/village-fund-directory/village-fund-directory.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./village-fund-directory.scss";
+
+const VillageFundDirectory = () => {
+  return <div>this is village fund</div>;
+};
+
+export default VillageFundDirectory;
--- src/views/admin/routes/index.js
+++ src/views/admin/routes/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import NewsManage from "../components/news-manage/news-manage";
+import VillageFundDirectory from "../components/village-fund-directory/village-fund-directory";
 
 const ManagementRoutes = () => {
   return (
     <Switch>
       <Route exact path="/management" component={NewsManage} />
       <Route exact path="/management/news-manage" component={NewsManage} />
+      <Route exact path="/management/village-fund-directory" component={VillageFundDirectory} />
     </Switch>
   );
 };
