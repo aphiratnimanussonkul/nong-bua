3db2a2f1f74588233009a7e9581d631e84b0c23e
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-16 17:26:43 +0700

    create confirm modal

---
+++ src/components/confirm-modal/confirm-modal.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import "./confirm-modal.scss";
+import { Divider, Button, Modal } from "@material-ui/core";
+
+const ConfirmModal = ({ title, descrption, onConfirm, onCancel }) => {
+  return (
+    <>
+      <Modal open={true} onClose={onCancel}>
+        <div className="modal">
+          <h3 className="toppick">{title}</h3>
+          <Divider />
+          <div className="detail">
+            {descrption.map((el) => (
+              <div className="row">
+                <div className="col title">
+                  <p>{el.title} : </p>
+                </div>
+                <div className="col">
+                  <p>{el.detail}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <Divider />
+          <div className="row action-button">
+            <Button
+              size="small"
+              variant="outlined"
+              className="green-solid-button"
+              onClick={onCancel}
+            >
+              ยกเลิก
+            </Button>
+            <Button
+              size="small"
+              variant="outlined"
+              className="brown-yellow-outlined-button"
+              onClick={onConfirm}
+            >
+              ลบข่าวนี้
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default ConfirmModal;
