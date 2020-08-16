2527bd3eb9c57a8e29a8a881f50c9a4152b40ebe
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-16 17:28:07 +0700

    add state loading

--- src/reducers/home/index.js
+++ src/reducers/home/index.js
@@ -7,25 +7,29 @@ export const initState = {
   news: [],
   villageIntroduce: null,
   error: null,
+  isLoading: true,
 };
 
 export default (state = initState, { type, payload, error }) => {
   switch (type) {
     case (FETCH_NEWS_INFORMATION.PENDDING, FETCH_VILLAGE_INTRODUCE.PENDDING):
-      return state;
+      return { ...state, isLoading: true };
     case FETCH_NEWS_INFORMATION.SUCCESS:
       return {
         ...state,
         news: payload,
+        isLoading: false,
       };
     case FETCH_VILLAGE_INTRODUCE.SUCCESS:
       return {
         ...state,
+        isLoading: false,
         villageIntroduce: payload[0],
       };
     case (FETCH_NEWS_INFORMATION.FAILED, FETCH_VILLAGE_INTRODUCE.FAILED):
       return {
         ...state,
+        isLoading: false,
         error,
       };
     default:
