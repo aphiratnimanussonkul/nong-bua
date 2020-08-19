825cb90605451e216072efda6ffe34a666026182
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-19 21:56:11 +0700

    check user uid is exist go to management page if not go to login page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import Login from "../views/login/login";
 import { connect } from "react-redux";
 import AppViewer from "../views/app-viewer/app-viewer";
 import Admin from "../views/admin/admin";
-const AppRoute = ({ isAdmin }) => {
+import { getUserInfo } from "../actions/user";
+const AppRoute = ({ dispatch, isAdmin }) => {
+  useEffect(() => {
+    dispatch(getUserInfo());
+  }, [dispatch]);
   return (
     <Switch>
       <Route exact path="/management/login" component={Login} />
