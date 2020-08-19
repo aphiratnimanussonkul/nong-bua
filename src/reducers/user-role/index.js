b91de2e49c90462a0d260d4313ef835a89be99f8
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-19 21:55:35 +0700

    handle fetch user info

--- src/reducers/user-role/index.js
+++ src/reducers/user-role/index.js
@@ -1,26 +1,31 @@
-import { SIGNIN_USER } from "../../actions/user";
+import { SIGNIN_USER, FETCH_USER_INFO } from "../../actions/user";
 
 export const initState = {
   isAdmin: false,
   loginFail: false,
   userInfo: null,
+  error: null,
 };
 
 export default (state = initState, { type, payload, error }) => {
   switch (type) {
-    case SIGNIN_USER.PENDDING:
+    case (SIGNIN_USER.PENDDING, FETCH_USER_INFO.PENDDING):
       return state;
-    case SIGNIN_USER.SUCCESS:
+    case (SIGNIN_USER.SUCCESS, FETCH_USER_INFO.SUCCESS):
       return {
         isAdmin: true,
         loginFail: false,
         userInfo: payload,
       };
+
+    case FETCH_USER_INFO.FAILED:
+      return initState;
     case SIGNIN_USER.FAILED:
       return {
         isAdmin: false,
         loginFail: true,
         userInfo: null,
+        error,
       };
     default:
       return state;
