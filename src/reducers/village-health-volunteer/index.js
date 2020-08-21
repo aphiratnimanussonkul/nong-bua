59056f0fe856adebb286efee5c5084a071a750ab
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-21 21:52:50 +0700

    add isLoadingState check

--- src/reducers/village-health-volunteer/index.js
+++ src/reducers/village-health-volunteer/index.js
@@ -1,6 +1,6 @@
 import {
   FETCH_VILLAGE_HEALTH_VOLUNTEER_NEWS,
-  FETCH_VILLAGE_HEALTH_VOLUNTEER_DIRECTORT,
+  FETCH_VILLAGE_HEALTH_VOLUNTEER_DIRECTORY,
   FETCH_VILLAGE_STATIC,
 } from "../../actions/village-health-volunteer";
 
@@ -9,24 +9,25 @@ export const initState = {
   error: null,
   directories: [],
   villageStatics: [],
+  isLoading: true,
 };
 
 export default (state = initState, { type, payload, error }) => {
   switch (type) {
     case (FETCH_VILLAGE_HEALTH_VOLUNTEER_NEWS.PENDDING,
-    FETCH_VILLAGE_HEALTH_VOLUNTEER_DIRECTORT.PENDDING,
+    FETCH_VILLAGE_HEALTH_VOLUNTEER_DIRECTORY.PENDDING,
     FETCH_VILLAGE_STATIC.PENDDING):
       return state;
     case FETCH_VILLAGE_HEALTH_VOLUNTEER_NEWS.SUCCESS:
-      return { ...state, news: payload };
-    case FETCH_VILLAGE_HEALTH_VOLUNTEER_DIRECTORT.SUCCESS:
-      return { ...state, directories: payload };
+      return { ...state, news: payload, isLoading: false };
+    case FETCH_VILLAGE_HEALTH_VOLUNTEER_DIRECTORY.SUCCESS:
+      return { ...state, directories: payload, isLoading: false };
     case FETCH_VILLAGE_STATIC.SUCCESS:
-      return { ...state, villageStatics: payload };
+      return { ...state, villageStatics: payload, isLoading: false };
     case (FETCH_VILLAGE_HEALTH_VOLUNTEER_NEWS.FAILED,
-    FETCH_VILLAGE_HEALTH_VOLUNTEER_DIRECTORT.FAILED,
+    FETCH_VILLAGE_HEALTH_VOLUNTEER_DIRECTORY.FAILED,
     FETCH_VILLAGE_STATIC.FAILED):
-      return { ...state, error };
+      return { ...state, error, isLoading: false };
     default:
       return state;
   }
