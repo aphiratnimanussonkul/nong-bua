279ac7577a7e598e5bc2055ee3c08fdd5611aff9
Aphirat Nimanussonkul <Tata@odds.team>
2020-08-24 22:33:33 +0700

    create function CUD about village

--- src/actions/about-village.js
+++ src/actions/about-village.js
@@ -15,10 +15,50 @@ export const getAboutVillage = () => async (dispatch) => {
       .then((result) => {
         dispatch({
           type: FETCH_ABOUT_VILLAGE.SUCCESS,
-          payload: result.docs.map((data) => data.data()),
+          payload: result.docs.map((data) => {
+            return {
+              id: data.id,
+              ...data.data(),
+            };
+          }),
         });
       });
   } catch (error) {
     dispatch({ type: FETCH_ABOUT_VILLAGE.FAILED });
   }
+};
+
+export const getAllAboutVillage = () => async (dispatch) => {
+  dispatch({ type: FETCH_ABOUT_VILLAGE.PENDDING });
+  try {
+    await firestore
+      .collection("about-village")
+      .get()
+      .then((result) => {
+        dispatch({
+          type: FETCH_ABOUT_VILLAGE.SUCCESS,
+          payload: result.docs.map((data) => {
+            return {
+              id: data.id,
+              ...data.data(),
+            };
+          }),
+        });
+      });
+  } catch (error) {
+    dispatch({ type: FETCH_ABOUT_VILLAGE.FAILED });
+  }
+};
+
+export const updateAboutVillageById = async (aboutVillage) => {
+  const { id, ...others } = aboutVillage;
+  return await firestore.collection("about-village").doc(id).set(others);
+};
+
+export const deleteAboutVillageById = async (id) => {
+  return await firestore.collection("about-village").doc(id).delete();
+};
+
+export const createAboutVillage = async (aboutVillage) => {
+  return await firestore.collection("about-village").add(aboutVillage);
 };
